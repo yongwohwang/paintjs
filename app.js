3f638b4caa89dabfbc57c1bcb91a1c63f7f4259b
yongwohwang <75257106+yongwohwang@users.noreply.github.com>
2021-06-04 14:15:08 +0700

    Jun3

--- app.js
+++ app.js
@@ -3,17 +3,28 @@ const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("jsColor");
 const brushSize = document.getElementById("jsRange");
 const mode = document.getElementById("jsMode");
+const saveBtn = document.getElementById("jsSave");
 
-canvas.width = 500;
-canvas.height = 500;
-ctx.strokeStyle = "black";
+const INITIAL_COLOR = "black"
+const CANVAS_SIZE = 500;
+
+canvas.width = CANVAS_SIZE;
+canvas.height = CANVAS_SIZE;
+
+ctx.fillStyle = "white";
+ctx.fillRect(0,0,canvas.width, canvas.height)
+
+ctx.strokeStyle = INITIAL_COLOR;
+ctx.fillStyle = INITIAL_COLOR;
+
 ctx.lineWidth = 2.5;
-
 let painting = false;
 let filling = false;
 
 function startPainting(){
-    painting = true;
+    if (filling===false){
+        painting = true;
+    }
 }
 function stopPainting(event){
     painting=false;
@@ -43,10 +54,30 @@ function handleModeClick(){
 function handleColorClick(event){
     const color = event.target.style.backgroundColor;
     ctx.strokeStyle = color;
+    ctx.fillStyle = color;
+
 }
 function handleBrushSize(event){
     console.log(event.target.value);
     ctx.lineWidth = parseInt(event.target.value);
+}
+function handleCanvasClick(event){
+    if(filling){
+        ctx.fillRect(0,0,canvas.width, canvas.height)
+    } else{
+
+    }
+}
+function handleCM(event){
+    event.preventDefault();
+}
+
+function handleSaveClick(){
+    const image = canvas.toDataURL();
+    const link = document.createElement("a");
+    link.href = image;
+    link.download = "PaintJS😀";
+    link.click();
 }
 
 if (canvas){
@@ -54,6 +85,12 @@ if (canvas){
     canvas.addEventListener("mousedown", startPainting);
     canvas.addEventListener("mouseup", stopPainting);
     canvas.addEventListener("mouseleave", stopPainting);
+    canvas.addEventListener("click",handleCanvasClick);
+    canvas.addEventListener("contextmenu", handleCM);
+}
+
+if (saveBtn){
+    saveBtn.addEventListener("click", handleSaveClick);
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
